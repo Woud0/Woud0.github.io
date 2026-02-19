f97bd3e4ecb87fc83ab94184f1702c13cec4d942
Wout Lesschaeve <wout.lesschaeve@student.hogent.be>
2026-02-19 10:28:49 +0100

    beginnend probeersel

---
+++ meerwolven/js/script.js
@@ -0,0 +1,16 @@
+let dorpsoudste, aantalLevens, spiekendMeisje, kleineWilde, idool, ziener, logee, logeertBij, weerwolf, vermoord, heks, dief;
+
+function getCurrentRoles() {
+    dorpsoudste = document.getElementById("dorpsoudste").value;
+    aantalLevens = document.getElementById("aantal-levens").value;
+    spiekendMeisje = document.getElementById("spiekend-meisje").value;
+    kleineWilde = document.getElementById("kleine-wilde").value;
+    idool = document.getElementById("idool").value;
+    ziener = document.getElementById("ziener").value;
+    logee = document.getElementById("logee").value;
+    logeertBij = document.getElementById("bij").value;
+    weerwolf = document.getElementById("weerwolf").value;
+    vermoord = document.getElementById("vermoord").value;
+    heks = document.getElementById("heks").value;
+    dief = document.getElementById("dief").value;
+}
